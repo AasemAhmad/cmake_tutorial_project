1ed302af5812f93a83d29224f8736ab7e033a098
Aasem Ahamd <engh.asem@gmail.com>
2018-10-15 15:31:00 +0200

    first success build using cmake and compiler g++ given by cygwin

--- application/src/build.cmd
+++ application/src/build.cmd
@@ -1,13 +1,12 @@
-
+@echo on
 set OLD_PATH=%PATH%
 
 set SRC_PATH=%~dp0
-set BUILD_PATH=%~dp0\build_win\
-set TOOLS_PATH=%SRC_PATH%\..\tools\
+set BUILD_PATH=%SRC_PATH%build_win\
+set TOOLS_PATH=%SRC_PATH%..\..\tools\
 set BIN_PATH=%TOOLS_PATH%win_build_tools\bin\
 set CMAKE_BIN_PATH=%TOOLS_PATH%cmake\bin\
-set GCC_BIN_PATH=%TOOLS_PATH%gcc-arm\bin\
-
+set GCC_BIN_PATH=C:\cygwin64\bin\
 
 set PATH=%BIN_PATH%;%CMAKE_BIN_PATH%;%GCC_BIN_PATH%;%PATH%
 
@@ -15,12 +14,12 @@ set PATH=%BIN_PATH%;%CMAKE_BIN_PATH%;%GCC_BIN_PATH%;%PATH%
 if not exist %BUILD_PATH% (
     mkdir %BUILD_PATH%
     cd %BUILD_PATH%
-    cmake -G "Unix Makefiles" ..
+    cmake -G "Unix Makefiles" -D CMAKE_TOOLCHAIN_FILE=../../../io/cmake/compiler.cmake ..
 ) ELSE (
     cd %BUILD_PATH%
 )
 make
 
-rem set PATH=%OLD_PATH%
+set PATH=%OLD_PATH%
 
 cd %SRC_PATH%
